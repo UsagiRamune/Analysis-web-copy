23238893f98a5906e2321fd7780fd7a647ef141b
K1zumi <ggotliliana@gmail.com>
2026-04-30 22:16:25 +0700

    Hotfix #3

--- ai-engagement-dashboard/src/features/upload/services/upload.api.ts
+++ ai-engagement-dashboard/src/features/upload/services/upload.api.ts
@@ -1,9 +1,9 @@
-import { mockCritiqueData } from "../../critique/data/mockData";
+import { mockData } from "../../critique/data/mockData"; 
 
 export const submitDesign = async () => {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve(mockCritiqueData);
+      resolve(mockData); 
     }, 1000);
   });
 };
